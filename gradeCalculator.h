b943aaaf87bb054598483acc105a64865d1b8926
Clawler920 <98423662+Clawler920@users.noreply.github.com>
2022-10-26 16:59:45 -0400

    Update gradeCalculator.h

--- gradeCalculator.h
+++ gradeCalculator.h
@@ -1,19 +1,17 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <string>
 #include <cmath>
 
 const int assignment_weight = 500, lab_weight = 50, final_exam_weight = 100, mec_project_weight = 30, final_project_weight = 350;
 
 class Grade{
-
-    private: 
-        int final_exam, mec_project, final_project;
-        std::vector<int> assignments, labs;
-
-    public:
-        Grade::Grade(std::vector<int> a, std::vector<int> l, int f_e, int mec_p, int f_p);
-        int calculateSingleGrade(int g);
-        int calculateMultipleGrades(std::vector<int> g);
-
+    private:
+        float final_exam, mec_project, final_project;
+        std::vector<float> assignments, labs;
+    public: 
+        Grade(std::vector<float> a, std::vector<float> l, float f_e, float mec_p, float f_p);
+        void calculateNumGrade();
+        void calculateLetterGrade();
 };
