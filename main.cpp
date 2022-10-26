1ef1d173fd7c09542f0faac6d4bc28864a27b6bc
Clawler920 <98423662+Clawler920@users.noreply.github.com>
2022-10-26 17:00:11 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
-#include "Chris.h"
-#include "Dom.h"
-#include "Matt.h"
-#include "Mubzy.h"
+#include "gradeCalculator.h"
 
 int main(){
+    
+    Grade chris({100, 100, 100, 100, 100}, {5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, 90, 30, 300);
+    Grade dom({80, 100, 95, 95, 80}, {5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, 85, 30, 300);
+    Grade matt({100, 90, 80, 80, 95}, {5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, 90, 30, 300);
+    Grade mubzy({80, 100, 80, 90, 100}, {5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, 80, 30, 300);
+
+    chris.calculateNumGrade();
+    dom.calculateNumGrade();
+    matt.calculateNumGrade();
+    mubzy.calculateNumGrade();
 
     return 0;
 }
