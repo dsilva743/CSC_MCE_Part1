5f9ed7c909f84bbd94233431cd324564c8a38fd1
Clawler920 <98423662+Clawler920@users.noreply.github.com>
2022-10-26 16:52:35 -0400

    Delete Mubzy.h

--- Mubzy.h
+++
@@ -1,6 +0,0 @@
-#include "gradeCalculator.h"
-
-class Mubzy : public Grade{
-    public:
-        Mubzy(std::vector<int> a, std::vector<int> l, int f_e, int mec_p, int f_p) : Grade(a, l, f_e, mec_p, f_p){};
-};
