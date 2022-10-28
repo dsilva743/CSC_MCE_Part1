1ac83e9326b15d91eac6b8bf4bf5900e2fa93d48
Dom Silva <90434787+dsilva743@users.noreply.github.com>
2022-10-28 11:44:38 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include "gradeCalculator.h"
 
 int main(){
-    
+
     Grade chris({100, 100, 100, 100, 100}, {5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, 90, 30, 300);
     Grade dom({80, 100, 95, 95, 80}, {5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, 85, 30, 300);
     Grade matt({100, 90, 80, 80, 95}, {5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, 90, 30, 300);
     Grade mubzy({80, 100, 80, 90, 100}, {5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, 80, 30, 300);
 
-    chris.calculateNumGrade();
-    dom.calculateNumGrade();
-    matt.calculateNumGrade();
-    mubzy.calculateNumGrade();
+    chris.calculateFinalGrade();
+    dom.calculateFinalGrade();
+    matt.calculateFinalGrade();
+    mubzy.calculateFinalGrade();
 
-    return 0;
-}
+    r
