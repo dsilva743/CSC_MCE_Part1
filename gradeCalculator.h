86f241fef0f8b5daefb7fd8ac2e7c6049c5aaae9
Dom Silva <90434787+dsilva743@users.noreply.github.com>
2022-10-28 11:44:15 -0400

    Update gradeCalculator.h

--- gradeCalculator.h
+++ gradeCalculator.h
@@ -4,15 +4,22 @@
 #include <string>
 #include <cmath>
 
-const int assignment_weight = 500, lab_weight = 50, final_exam_weight = 100, mec_project_weight = 30, final_project_weight = 350;
+
+#ifndef MCE_01_GRADECALCULATOR_H
+#define MCE_01_GRADECALCULATOR_H
+
 
 class Grade{
+
     private:
         float final_exam, mec_project, final_project;
         std::vector<float> assignments, labs;
-    public: 
+    public:
         Grade(std::vector<float> a, std::vector<float> l, float f_e, float mec_p, float f_p);
         int calculateFinalGrade();
-        char calculateLetterGrade();
+        std::string calculateLetterGrade();
         double calculateMidTermGrade();
 };
+
+
+#endif //MCE_01_GRADECALCULATOR_H
