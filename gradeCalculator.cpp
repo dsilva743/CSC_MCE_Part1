ea23d37ff5cd685d60a5ab2906df7e2569d17c58
Dom Silva <90434787+dsilva743@users.noreply.github.com>
2022-10-28 11:43:45 -0400

    Update gradeCalculator.cpp

--- gradeCalculator.cpp
+++ gradeCalculator.cpp
@@ -1,14 +1,16 @@
+#include <iostream>
 #include "gradeCalculator.h"
 
 Grade::Grade(std::vector<float> a, std::vector<float> l, float f_e, float mec_p, float f_p){
-    this->assignments = a;
-    this->labs = l;
-    this->final_exam = f_e;
-    this->mec_project = mec_p;
-    this->final_project = f_p;
+    this-> assignments = a;
+    this-> labs = l;
+    this-> final_exam = f_e;
+    this-> mec_project = mec_p;
+    this-> final_project = f_p;
 }
 
-int Grade::calculateFinalGrade(){    
+//Calculate final grade
+int Grade::calculateFinalGrade(){
 
     float assignmentPts = 0;
     float labPts = 0;
@@ -19,53 +21,74 @@ int Grade::calculateFinalGrade(){
     for(int i = 0; i < assignments.size(); i++){
         assignmentPts += assignments[i];
     }
-    
+
     for(int i = 0; i < labs.size(); i++){
         labPts += labs[i];
     }
 
     float totalPts = (assignmentPts + labPts + finalExamPts + mecProjectPts + finalProjectPts) / 10;
-    
+
     return totalPts;
 }
 
-char Grade::calculateLetterGrade(float totalPts){
-    char letter;
-    if (totalPts < 60){
-        letter = 'F';
+
+//Output letter grade based off final grade
+std::string Grade::calculateLetterGrade(float totalPts){
+
+    std::string letter;
+
+    if(totalPts >= 94){
+        letter = "A";
     }
-	else if (totalPts < 70){
-        letter = 'D';
+    else if(totalPts < 94 && totalPts >= 90){
+        letter = "A-";
     }
-	else if (totalPts < 80){
-        letter = 'C';
-    }	
-	else if (totalPts < 90){
-        letter = 'B';
+    else if(totalPts < 90 && totalPts >= 87){
+        letter = "B+";
     }
-	else{
-        letter = 'A';
+    else if(totalPts < 87 && totalPts >= 83){
+        letter = "B";
+    }
+    else if(totalPts < 83 && totalPts >= 80){
+        letter = "B-";
+    }
+    else if(totalPts < 80 && totalPts >= 77){
+        letter = "C+";
+    }
+    else if(totalPts < 77 && totalPts >= 73){
+        letter = "C";
+    }
+    else if(totalPts < 73 && totalPts >= 70){
+        letter = "C-";
+    }
+    else if(totalPts < 70 && totalPts >= 67){
+        letter = "D+";
+    }
+    else if(totalPts < 67 && totalPts >= 60){
+        letter = "D";
+    }
+    else{
+        letter = "F";
     }
 
     return letter;
-
-   }
-    
 }
 
-double Grade::calculateMidTermGrade(){
+
+//Calculating mid term grade 3 assignments 6 labs
+double Grade::calculateMidTermGrade(std::vector<float> a, std::vector<float> l){
 
     double assignmentTotal = 0;
     double labTotal = 0;
 
     //Total sum of the first 3 assignments grades
     for(int i = 0; i < 3; i++){
-        assignmentTotal += assignment[i];
+        assignmentTotal += assignments[i];
     }
 
     //Total sum of for 6 lab assignment grades
     for(int i = 0; i < 6; i++){
-        labTotal += lab[i];
+        labTotal += labs[i];
     }
 
 
